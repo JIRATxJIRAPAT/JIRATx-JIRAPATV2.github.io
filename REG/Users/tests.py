9abf6030dbc4e9e0da07b53bb1c65cb5ad1b3a87
Jirat Sukphalanon <6210612633@cn330>
2021-10-02 23:39:41 +0700

    complete as4

--- REG/Users/tests.py
+++ REG/Users/tests.py
@@ -30,25 +30,27 @@ class userViewTestCase(TestCase):
         self.assertEqual(response.status_code , 302)
 
     def test_login_view_successful(self):
-        """ login successful """
+        """ login successful redirect to user page"""
         user = User.objects.get(username = "user1")
         c = Client()
         response = c.post(reverse('Users:login') , {'username': 'user1' , 'password': '123'})
         self.assertEqual(response.status_code, 302 )
     
     def test_login_view_unsuccessful(self):
-        """ login unsuccessful """
+        """ login unsuccessful need login again """
         user = User.objects.get(username = "user1")
         c = Client()
         response = c.post(reverse('Users:login') , {'username': 'user1' , 'password': '069'})
         self.assertEqual(response.status_code, 200 )
         
     def test_login_view(self):
+        """ login view is ok"""
         c = Client()
         response = c.get(reverse("Users:login"))
         self.assertEqual(response.status_code , 200)
 
     def test_logout_view(self):
+        """ logout is worked redirect to login page"""
         c = Client()
         response = c.get(reverse("Users:logout"))
         self.assertEqual(response.status_code , 302)
